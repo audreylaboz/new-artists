1193228266b0a9d8e298601f055aa57a562a7b62
Audrey Laboz <audrey.laboz@mdycehs.net>
2017-03-23 15:37:54 +0000

    first commit

---
+++ p5/sketch.js
@@ -0,0 +1,32 @@
+function setup () {
+    createCanvas(900,900)
+}
+
+function draw() {
+    stroke(0, 97, 255);
+    strokeWeight(10); 
+    fill(255, 255, 255, 1);
+    ellipse (150, 150, 160, 160);
+    
+    stroke(0, 0, 0);
+    strokeWeight(10); 
+    fill(255, 255, 255, 1);
+    ellipse (330, 150, 160, 160);
+    
+    stroke(255, 0, 34);
+    strokeWeight(10); 
+    fill(255, 255, 255, 1);
+    ellipse (510, 150, 160, 160);
+    
+    stroke(0, 242, 0);
+    strokeWeight(10); 
+    fill(255, 255, 255, 1);
+    ellipse (420, 250, 160, 160);
+    
+    stroke(255, 242, 0);
+    strokeWeight(10); 
+    fill(255, 255, 255, 1);
+    ellipse (236, 250, 160, 160);
+    
+}
+
